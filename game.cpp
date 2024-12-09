ab4b488322f05f128c01340620bfbb031bcebe00
guilhermecsnpeixoto <guilhermecsnpeixoto@gmail.com>
2024-12-09 14:21:26 +0000

    implemented debugGame()

--- game.cpp
+++ game.cpp
@@ -21,14 +21,9 @@ std::vector<std::string> generateKey() {
 
 }
 
-int validatePlaytime(const int max_playtime) {
-    if (max_playtime < 1 || max_playtime > 600) {
-        return 1;
-    }
-    return 0;
-}
 
-int startGame(const int player_id, const int max_playtime, const char mode) {
+// INPUTS ARE ASSUMED TO BE CORRECT AND VALIDATED CLIENT SIDE
+int startGame(const int player_id, const int max_playtime) {
     currentGameFilename = "GAMES/GAME_" + std::to_string(player_id) + ".txt";
     std::ifstream file(currentGameFilename);
     
@@ -37,12 +32,8 @@ int startGame(const int player_id, const int max_playtime, const char mode) {
         std::ofstream newFile(currentGameFilename);
         std::vector<std::string> key = generateKey();
 
-        if (validatePlaytime(max_playtime)) {
-            return 1;
-        }
-
         newFile << std::to_string(player_id) << std::endl;
-        newFile << mode << std::endl;
+        newFile << "P" << std::endl;
         for (int i = 0; i < 4; i++) {
             newFile << key[i];
         }
@@ -72,12 +63,53 @@ int startGame(const int player_id, const int max_playtime, const char mode) {
     }
 }
 
-int quitGame(const int player_id) {
+int quitGame() {
     if (!currentGameFilename.empty()) {
         currentGameFilename.clear();
         return 0;
     }
     return 1;
+}
+
+int debugGame(const int player_id, const int max_playtime, const std::vector<std::string> key) {
+    
+    currentGameFilename = "GAMES/GAME_" + std::to_string(player_id) + ".txt";
+    std::ifstream file(currentGameFilename);
+    
+    if (!file) {
+
+        std::ofstream newFile(currentGameFilename);
+
+        newFile << std::to_string(player_id) << std::endl;
+        newFile << "D" << std::endl;
+        for (int i = 0; i < 4; i++) {
+            newFile << key[i];
+        }
+        newFile << std::endl;
+        newFile << std::to_string(max_playtime) << std::endl;
+
+        std::time_t currentTime = std::time(nullptr);
+        std::tm* localTime = std::localtime(&currentTime);
+
+        char date[11];
+        std::strftime(date, sizeof(date), "%Y-%m-%d", localTime);
+
+        newFile << date << std::endl;
+
+        char time[9];
+        std::strftime(time, sizeof(time), "%H:%M:%S", localTime);
+
+        newFile << time << std::endl;
+
+        newFile << std::to_string(currentTime) << std::endl;
+
+        newFile.close();
+    
+        return 0;
+    } else {
+        return 1;
+    }
+    
 }
 
 
