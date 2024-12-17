b9a7c12f19787011360bcf529b4f7073be382b07
henriquessss <gffthenriques@gmail.com>
2024-12-17 15:39:12 +0000

    game.h corrigido

--- game.h
+++ game.h
@@ -8,6 +8,16 @@
 
 namespace Game {
 
+    // Struct SCORELIST
+    struct SCORELIST {
+        int score[10];
+        std::string PLID[10];
+        std::string col_code[10];
+        int no_tries[10];
+        std::string mode[10];
+        int n_scores;
+    };
+
     /**
      * @brief Generates a random key for the game.
      * 
@@ -22,7 +32,36 @@ namespace Game {
      * @param max_playtime The maximum playtime allowed for the game.
      * @return int 0 if the game is successfully started, 1 if the game already exists.
      */
-    int startGame(const int player_id, const int max_playtime);
+    std::vector<std::string> startGame(const int player_id, const int max_playtime);
+
+    /**
+     * @brief Processes a play attempt.
+     * 
+     * @param guess The guess made by the player.
+     * @return std::string The status of the game after the attempt.
+     */
+    std::vector<std::string> playAttempt(int plid, const std::vector<std::string>& guess);
+
+    /**
+     * @brief Returns a vector of strings with every try in the game file.
+     * 
+     * @return std::vector<std::string> A vector containing all the tries.
+     */
+    std::vector<std::string> showTrials(int player_id);
+
+    /**
+     * @brief Returns a integer of the number of scores found for that player
+     * 
+     * @return int 
+     */
+    int FindPlayerTopScores(SCORELIST list, int plid);
+
+    /**
+     * @brief Returns a vector of strings with the top 10 scores including PLID, number of plays to win, secret key
+     * 
+     * @return std::vector<std::string> A vector containing the top 10 scores
+     */
+    std::vector<std::string> viewScoreboard(int player_id);
 
     /**
      * @brief Quits the current game.
@@ -57,20 +96,6 @@ namespace Game {
      */
     void createDirectory(const std::string& path);
 
-    /**
-     * @brief Processes a play attempt.
-     * 
-     * @param guess The guess made by the player.
-     * @return std::string The status of the game after the attempt.
-     */
-    std::string playAttempt(int plid, const std::vector<std::string>& guess);
-
-    /**
-     * @brief Returns a vector of strings with every try in the game file.
-     * 
-     * @return std::vector<std::string> A vector containing all the tries.
-     */
-    std::vector<std::string> showTrials();
 }
 
 #endif // GAME_H
